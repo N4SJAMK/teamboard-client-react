de9775be25caa0789e90aa3f3cf0a7e66a81bd1e
Teemu Kontio <teemu@t-palvelut.net>
2015-06-23 11:17:13 +0300

    Edited some styling

--- src/scripts/components/dialog/view-about.js
+++ src/scripts/components/dialog/view-about.js
@@ -51,33 +51,42 @@ export default React.createClass({
                     About Contriboard
                 </section>
                 <section className="dialog-content">
-                    <div className="aboutTable"> 
+                    <div className="description-area">
+                        Fast open-source brainstorming tool for everyone.
+                    </div>
+                    <hr className="divider" />
+                    <div className="aboutTable">
                         <div className="aboutTableRow">
-                            <div className="aboutTableCell-left">Check our homepages:</div>
+                            <div className="aboutTableCell-left">Developers:</div>
                             <div className="aboutTableCell">
-                                <a target="_blank" href="http://n4sjamk.github.io/">http://n4sjamk.github.io/</a>
+                                <a target="_blank" href="http://n4sjamk.github.io/">n4sjamk.github.io</a>
                             </div>
                         </div>
-
                         <div className="aboutTableRow">
-                            <div className="aboutTableCell-left">Client version:</div>
-                            <div className="aboutTableCell">{this.state.clientVersion}</div>
+                            <div className="aboutTableCell-left">Contriboard homepage:</div>
+                            <div className="aboutTableCell">
+                                <a target="_blank" href="http://n4sjamk.github.io/contriboard">n4sjamk.github.io/contriboard</a>
+                            </div>
                         </div>
-
-                        <div className="aboutTableRow">
-                            <div className="aboutTableCell-left">API version:</div>
-                            <div className="aboutTableCell">{this.state.apiVersion}</div>
+                    </div>
+                    <div className="versionTable">
+                        <div className="versionTableRow">
+                            <div className="versionTableCell-left">Client version:</div>
+                            <div className="versionTableCell">{this.state.clientVersion}</div>
                         </div>
-
-                        <div className="aboutTableRow">
-                            <div className="aboutTableCell-left">IMG version:</div>
-                            <div className="aboutTableCell">{this.state.imgVersion}</div>
+                        <div className="versionTableRow">
+                            <div className="versionTableCell-left">API version:</div>
+                            <div className="versionTableCell">{this.state.apiVersion}</div>
+                        </div>
+                        <div className="versionTableRow">
+                            <div className="versionTableCell-left">IMG version:</div>
+                            <div className="versionTableCell">{this.state.imgVersion}</div>
                         </div>
                     </div>
                 </section>
                 <section className="dialog-footer">
                 	<button className="btn-primary" onClick={this.close}>
-						Done
+						Close
 					</button>
                 </section>
             </Dialog>
