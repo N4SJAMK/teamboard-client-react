12e8176063d9d63527abc1eae71cdf61cc4ba630
Teemu Kontio <teemu@t-palvelut.net>
2015-06-24 09:31:51 +0300

    Fix lint errors

--- src/scripts/utils/api.js
+++ src/scripts/utils/api.js
@@ -34,7 +34,7 @@ export default {
 	generateAccessCode: generateAccessCode,
 
 	queryApiVersion: queryApiVersion,
-	queryImgVersion: queryImgVersion 
+	queryImgVersion: queryImgVersion
 }
 
 const API_URL = process.env.API_URL || 'http://localhost:9002/api';
